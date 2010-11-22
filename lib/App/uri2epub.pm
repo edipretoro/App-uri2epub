a43a0f6f5dd80f4ae9b209335adc2f58187f6b20
Emmanuel Di Pretoro <edipretoro@gmail.com>
2010-11-22 15:45:26 +0100

    * lib/App/uri2epub.pm: add a constructor, new()

--- lib/App/uri2epub.pm
+++ lib/App/uri2epub.pm
@@ -2,4 +2,31 @@ use strict;
 use warnings;
 package App::uri2epub;
 # ABSTRACT: a simple application to transform a URL into an ePub file.
+
+=meth new
+
+The constructor of App::uri2epub. 
+
+You can use the following parameters:
+
+=over
+
+=item uri
+
+The URL to retrieve and process.
+
+=item output_file
+
+The filename of the output.
+
+=back
+
+=cut
+
+sub new {
+    my ($class, %args) = @_;
+    my $self = {};
+
+    return $class, $self;
+}
 1;
